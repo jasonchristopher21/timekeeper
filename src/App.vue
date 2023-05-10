385744197c31f974fcd9db0c1fd19758e3e9383c
Jason Christopher <jason.christopher@u.nus.edu>
2023-05-10 22:25:27 +0800

    Connect Tailwind to Project

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import "./style.css"
 </script>
 
 <template>
-  
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </template>
 
 <!-- <template>
