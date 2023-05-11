7651249e9cd4e44798ed74a9335c8adc91e922a8
Jason Christopher <jason.christopher@u.nus.edu>
2023-05-11 22:19:15 +0800

    feat: add dashboard cards

---
+++ src/components/DashboardCard.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import "../style.css"
+import styles from '../style';
+</script>
+
+<template>
+    <div>
+        <div class="bg-white w-full rounded-xl grid lg:grid-cols-3 sm:grid-cols-2 gap-3">
+            <div class="image">
+                <div class="bg-orange w-full h-full p-5 rounded-xl">
+                </div>
+            </div>
+            <div class="col-span-2 p-5">
+                <div class="text-2xl text-black font-jakarta font-bold">
+                    Presentation 1
+                </div>
+                <div class="text-lg text-dgray mt-2 font-lato font-semibold">
+                    17 June 2023
+                </div>
+                <div class="text-sm text-dgray mt-2 font-lato font-regular opacity-60 italic">
+                    Last modified 17 April 2023
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,20 +1,30 @@
 <script lang="ts">
+import DashboardCard from "../components/DashboardCard.vue"
 export default {
   data() {
     return {
       name: "Jason"
     }
+  },
+  components: {
+    DashboardCard,
   }
 }
 </script>
 
 <template>
   <div class="px-10 py-10">
-    <div class="text-4xl text-black font-jakarta font-bold">
-      Hello, {{ name }}
+    <div class="header">
+      <div class="text-4xl text-black font-jakarta font-bold">
+        Hello, {{ name }}
+      </div>
+      <div class="text-xl text-dgray mt-2 font-jakarta font-semibold ">
+        Ready to timekeep your presentation today? :D
+      </div>
     </div>
-    <div class="text-xl text-dgray mt-2 font-jakarta font-semibold ">
-      Ready to timekeep your presentation today? :D
+    <div class="grid grid-cols-3 gap-5 mt-10">
+      <DashboardCard />
+      <DashboardCard />
     </div>
   </div>
 </template>
